e3582c1c1090161b1499621a4c657cf85d5c9c66
Jose Gonzalez <jsgonzalez903@gmail.com>
2018-08-17 00:59:40 -0400

    Actualizacion de RMN_H_dibujar.py

--- Analizar y dibujar espectros RMN/H/RMN_H_dibujar.py
+++ Analizar y dibujar espectros RMN/H/RMN_H_dibujar.py
@@ -13,7 +13,7 @@ job_TMS = open(log, 'r')
 cont=0
 SHIELDING_TMS = []
 aq_TMS = 0
-RMN_13C_TMS =[]
+RMN_H_TMS =[]
 suma_TMS = 0
 
 lines = job_TMS.readlines()
@@ -23,15 +23,15 @@ for line in lines:
 	if(cont == aq_TMS):
 		#uno = line
 		SHIELDING_TMS = SHIELDING_TMS + line.split()
-	if ("C         X" in line):
+	if ("H         X" in line):
 		aq_TMS=cont+3
 		#print(val)
 
 for i in range(0, len(SHIELDING_TMS)):		
-	RMN_13C_TMS.append(float(str(SHIELDING_TMS[i])))
-	suma_TMS = RMN_13C_TMS[i] + suma_TMS
+	RMN_H_TMS.append(float(str(SHIELDING_TMS[i])))
+	suma_TMS = RMN_H_TMS[i] + suma_TMS
 	
-valor_TMS = suma_TMS/len(RMN_13C_TMS)
+valor_TMS = suma_TMS/len(RMN_H_TMS)
 #print(valor_TMS)
 
 
@@ -47,7 +47,7 @@ job = open(log, 'r')
 cont=0
 SHIELDING = []
 aq = 0
-RMN_13C =[]
+RMN_H =[]
 
 lines = job.readlines()
 
@@ -56,50 +56,50 @@ for line in lines:
 	if(cont == aq):
 		#uno = line
 		SHIELDING = SHIELDING + line.split()
-	if ("C         X" in line):
+	if ("H         X" in line):
 		aq=cont+3
 		#print(val)
 		
 print('----------------------------------------------------------')
-cadena = "Valores en ppm 13C " + log
+cadena = "Valores en ppm H " + log
 print(                cadena.center(50, " ")                 )
 print('----------------------------------------------------------')
 print('----------------------------------------------------------')
 	
 for i in range(0, len(SHIELDING)):		
-	RMN_13C.append(float(str(SHIELDING[i])))
-	if(RMN_13C[i] <= valor_TMS):
-		RMN_13C[i] = valor_TMS - RMN_13C[i]
-	else: RMN_13C[i] = RMN_13C[i] - valor_TMS
-	print("\t \t" + str(round(RMN_13C[i],3)) + ' ppm         ')
+	RMN_H.append(float(str(SHIELDING[i])))
+	if(RMN_H[i] <= valor_TMS):
+		RMN_H[i] = valor_TMS - RMN_H[i]
+	else: RMN_H[i] = RMN_H[i] - valor_TMS
+	print("\t \t" + str(round(RMN_H[i],3)) + ' ppm         ')
 	print()
 	
 print('----------------------------------------------------------')
-#print(RMN_13C)
+#print(RMN_H)
 
 job.close()
 
 question = input("Quiere guardar estos valores y/n ")
 if(question== 'y'):
 	name = log.strip('.log')
-	work = ("Valores en ppm 13C " + name) + '.txt'
+	work = ("Valores en ppm H " + name) + '.txt'
 	#print(work)
 	doc = open(work , 'w')
 	doc.write('ppm\n')
-	RMN_13C.sort(reverse=True)
-	for i in range(0, len(RMN_13C)):
-		doc.write(str(round(RMN_13C[i],3)))
+	RMN_H.sort(reverse=True)
+	for i in range(0, len(RMN_H)):
+		doc.write(str(round(RMN_H[i],3)))
 		doc.write('\n')
 	print(" \nLISTO ")
 	doc.close()
 
 
-#RMN_13C.sort()
+#RMN_H.sort()
 
-#x =[RMN_13C[0],RMN_13C[0]]
+#x =[RMN_H[0],RMN_H[0]]
 #y = [0, 1000]
 
-#z =[RMN_13C[1],RMN_13C[1]]
+#z =[RMN_H[1],RMN_H[1]]
 #w = [0, 212]
 
 #l = [-5,200]
